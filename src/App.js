fdde62f67d261b61e1d33ab6ac031a652af44b1c
letitia-chiu <hsuan.acc@outlook.com>
2024-01-27 13:49:17 +0800

    support-dark-theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useState } from 'react'
+
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg'
 import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg'
 import { ReactComponent as RainIcon } from './images/rain.svg'
 import { ReactComponent as RefreshIcon } from './images/refresh.svg'
+
 import styled from '@emotion/styled'
+import { ThemeProvider } from '@emotion/react'
+
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: '#121416',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#f9f9fa',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
+};
 
 const Container = styled.div`
-  background-color: #ededed;
+  background-color: ${({ theme }) => theme.backgroundColor};
   height: 100%;
   display: flex;
   align-items: center;
@@ -15,17 +39,18 @@ const WeatherCard = styled.div`
   position: relative;
   padding: 30px 15px;
   min-width: 360px;
-  box-shadow: 0 1px 3px 0 #999999;
-  background-color: #f9f9f9;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
+  border-radius: 7px;
 `
 const Location = styled.div`
   font-size: 28px;
-  color: #212121;
+  color: ${({ theme }) => theme.titleColor};
   margin-bottom: 20px;
 `
 const Description = styled.div`
   font-size: 16px;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 30px;
 `
 const CurrentWeather = styled.div`
@@ -35,7 +60,7 @@ const CurrentWeather = styled.div`
   margin-bottom: 30px;
 `
 const Temperature = styled.div`
-  color: #757575;
+  color: ${({ theme }) => theme.temperatureColor};
   font-size: 96px;
   font-weight: 300;
   display: flex;
@@ -49,7 +74,7 @@ const AirFlow = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 20px;
 
   svg {
@@ -63,7 +88,7 @@ const Rain = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
 
   svg {
     width: 25px;
@@ -81,7 +106,7 @@ const Refresh = styled.div`
   font-size: 12px;
   display: inline-flex;
   align-items: flex-end;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
 
   svg {
     margin-left: 10px;
@@ -92,29 +117,32 @@ const Refresh = styled.div`
 `
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState('light')
 
   return (
-    <Container>
-      <WeatherCard>
-        <Location>台北市</Location>
-        <Description>多雲時晴</Description>
-        <CurrentWeather>
-          <Temperature>
-            23 <Celsius>°C</Celsius>
-          </Temperature>
-          <DayCloudy />
-        </CurrentWeather>
-        <AirFlow>
-          <AirFlowIcon /> 23 m/h
-        </AirFlow>
-        <Rain>
-          <RainIcon /> 48%
-        </Rain>
-        <Refresh>
-          最後觀測時間：上午 12:03 <RefreshIcon />
-        </Refresh>
-      </WeatherCard>
-    </Container>
+    <ThemeProvider theme={theme[currentTheme]}>
+      <Container>
+        <WeatherCard>
+          <Location>台北市</Location>
+          <Description>多雲時晴</Description>
+          <CurrentWeather>
+            <Temperature>
+              23 <Celsius>°C</Celsius>
+            </Temperature>
+            <DayCloudy />
+          </CurrentWeather>
+          <AirFlow>
+            <AirFlowIcon /> 23 m/h
+          </AirFlow>
+          <Rain>
+            <RainIcon /> 48%
+          </Rain>
+          <Refresh>
+            最後觀測時間：上午 12:03 <RefreshIcon />
+          </Refresh>
+        </WeatherCard>
+      </Container>
+    </ThemeProvider>
   )
 }
 
